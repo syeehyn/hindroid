c11fccad629d3c9cc158886e4cff19d9d9fe780f
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-03-08 19:17:55 -0700

    update run.py

--- run.py
+++ run.py
@@ -1,10 +1,19 @@
 import sys
 import json
 import os
-from src.datasets import get_data
+from src.datasets import *
+from src.utils import create_metadata
 from src.features import extract_benign
 from glob import glob
 def main(targets):
+    if 'metadata' in targets:
+        create_metadata()
+    if 'sampling' in targets:
+        cfg = json.load(open('./config/sample-params.json'))
+        sampling(**cfg)
+    if 'sampling-test' in targets:
+        cfg = cfg = json.load(open('./config/test-sample.json'))
+        sampling(**cfg)
     if 'data' in targets:
         cfg = json.load(open('./config/data-params.json'))
         get_data(**cfg)
