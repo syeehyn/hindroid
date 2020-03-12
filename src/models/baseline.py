e0937fba9248aa7812bcec719aba734912b55d2c
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-03-12 01:45:04 -0700

    update

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -9,11 +9,19 @@ from tqdm import tqdm
 from scipy import sparse
 import psutil
 from pathlib import Path
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, fbeta_score, accuracy_score, confusion_matrix
 NUM_WORKER = psutil.cpu_count(logical = False)
 ROOT_DIR = Path(__file__).parent.parent.parent
 FP_b = 'interim/b_features/*.csv'
 FP_m = 'interim/m_features/*.csv'
-
 def _preproc(test, FP_b, FP_m):
     client = Client(n_workers = NUM_WORKER)
     if test:
@@ -34,7 +42,8 @@ def _preproc(test, FP_b, FP_m):
                     {
                         'api': tunique,
                         'block': tunique,
-                        'package': tunique
+                        'package': tunique,
+                        'malware': 'mean'
                     } , split_out = NUM_WORKER)
     most_invo = df.groupby('app').invocation.apply(lambda x: x.mode()[0], meta = str)
     most_api = df.groupby('app').api.apply(lambda x: x.mode()[0], meta = str)
@@ -44,4 +53,46 @@ def _preproc(test, FP_b, FP_m):
     results['most_invo'] = result[0][1].tolist()
     results['most_api'] = result[0][2].tolist()
     results['most_package'] = result[0][3].tolist()
-    return result
+    return results
+def baseline(test, clf, df):
+    num_feat = ['api', 'block', 'package']
+    num_transformer = Pipeline(steps=[
+            ('scaler', StandardScaler())
+        ])
+    cat_feat = ['most_invo', 'most_api', 'most_package']
+    cat_transformer = Pipeline(steps=[
+            ('onehot', OneHotEncoder(handle_unknown='ignore'))
+        ])
+    preproc = ColumnTransformer(transformers=[('num', num_transformer, num_feat),\
+                                                ('cat', cat_transformer, cat_feat)])
+    pl = Pipeline(steps=[('preprocessor', preproc),
+            ('clf', clf)
+            ])
+    return pl
+def evaluate(test = False, clfs = [LogisticRegression(), SVC(), RandomForestClassifier()], test_size = .33):
+    df = _preproc(test, FP_b, FP_m)
+    X = df.drop('malware', axis = 1)
+    y = df.malware
+    X_train, X_test, y_train, y_test = \
+            train_test_split(X, y, test_size=test_size)
+    results = []
+    for clf in clfs:
+        model = baseline(test, clf, df)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        f1 = f1_score(y_test, y_pred)
+        # beta = fbeta_score(y_test, y_pred, average='binary', beta=1)
+        acc = accuracy_score(y_test, y_pred)
+        tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+        res = {
+                'method': type(clf).__name__,
+                'f1': f1,
+                # 'beta': beta,
+                'acc': acc,
+                'tp': tp,
+                'fp': fp,
+                'tn': tn,
+                'fn': fn
+                }
+        results.append(res)
+    return pd.DataFrame(results), len(X)
