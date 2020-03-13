55c0e5ed0b2baf0acadf19be89033a3c03c9acfa
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-03-13 14:54:18 -0700

    Update run.py

--- run.py
+++ run.py
@@ -9,55 +9,27 @@ from glob import glob
 def main(targets):
     if 'metadata' in targets:
         create_metadata()
-    if 'sampling' in targets:
-        cfg = json.load(open('./config/sample-params.json'))
-        sampling(**cfg)
     if 'sampling-test' in targets:
         cfg = cfg = json.load(open('./config/test-sample.json'))
         sampling(**cfg)
-    if 'data' in targets:
-        cfg = json.load(open('./config/data-params.json'))
-        get_data(**cfg)
+    if 'sampling' in targets:
+        cfg = json.load(open('./config/sample-params.json'))
+        sampling(**cfg)
     if 'data-test' in targets:
         cfg = json.load(open('./config/test-params.json'))
         get_data(**cfg)
-    if 'process' in targets:
-        fp = json.load(open('./config/data-params.json'))['m_dir']
-        extract_benign()
-        extract_malware(fp)
     if 'process-test' in targets:
         fp = json.load(open('./config/test-params.json'))['m_dir']
         extract_benign(True)
         extract_malware(fp, True)
-    if 'matrix' in targets:
-        construct_matrices(False, True, True, True)
     if 'matrix-test' in targets:
         construct_matrices(True, True, True, True)
-    if 'baseline' in targets:
-        result = baseline.evaluate(False)
-        print('training metrics: ')
-        print(result[0])
-        print('testing metrics: ')
-        print(result[1])
     if 'baseline-test' in targets:
         result = baseline.evaluate(True)
         print('training metrics: ')
         print(result[0])
         print('testing metrics: ')
         print(result[1])
-    if 'evaluate' in targets:
-        import pandas as pd
-        result = evaluate(False)
-        print('training metrics: ')
-        print(result[0])
-        print('testing metrics: ')
-        print(result[1])
-        try:
-            os.mkdir('./data/datasets/processed/results')
-        except:
-            pass
-        result[0].to_csv('./data/datasets/processed/results/training.csv', index = False)
-        result[1].to_csv('./data/datasets/processed/results/testing.csv', index = False)
     if 'evaluate-test' in targets:
         import pandas as pd
         result = evaluate(True)
@@ -71,24 +43,6 @@ def main(targets):
             pass
         result[0].to_csv('./data/tests/processed/results/training.csv', index = False)
         result[1].to_csv('./data/tests/processed/results/testing.csv', index = False)
-    if 'project' in targets:
-        cfg = json.load(open('./config/data-params.json'))
-        get_data(**cfg)
-        fp = json.load(open('./config/data-params.json'))['m_dir']
-        extract_benign(False)
-        extract_malware(fp, False)
-        construct_matrices(False, True, True, True)
-        result = evaluate(False)
-        try:
-            os.mkdir('./data/datasets/processed/results')
-        except:
-            pass
-        print('training metrics: ')
-        print(result[0])
-        print('testing metrics: ')
-        print(result[1])
-        result[0].to_csv('./data/datasets/processed/results/training.csv', index = False)
-        result[1].to_csv('./data/datasets/processed/results/testing.csv', index = False)
     if 'test-project' in targets:
         cfg = json.load(open('./config/test-params.json'))
         get_data(**cfg)
@@ -107,6 +61,52 @@ def main(targets):
         print(result[1])
         result[0].to_csv('./data/tests/processed/results/training.csv', index = False)
         result[1].to_csv('./data/tests/processed/results/testing.csv', index = False)
+    if 'data' in targets:
+        cfg = json.load(open('./config/data-params.json'))
+        get_data(**cfg)
+    if 'process' in targets:
+        fp = json.load(open('./config/data-params.json'))['m_dir']
+        extract_benign()
+        extract_malware(fp)
+    if 'matrix' in targets:
+        construct_matrices(False, True, True, True)
+    if 'baseline' in targets:
+        result = baseline.evaluate(False)
+        print('training metrics: ')
+        print(result[0])
+        print('testing metrics: ')
+        print(result[1])
+    if 'evaluate' in targets:
+        import pandas as pd
+        result = evaluate(False)
+        print('training metrics: ')
+        print(result[0])
+        print('testing metrics: ')
+        print(result[1])
+        try:
+            os.mkdir('./data/datasets/processed/results')
+        except:
+            pass
+        result[0].to_csv('./data/datasets/processed/results/training.csv', index = False)
+        result[1].to_csv('./data/datasets/processed/results/testing.csv', index = False)
+    if 'project' in targets:
+        cfg = json.load(open('./config/data-params.json'))
+        get_data(**cfg)
+        fp = json.load(open('./config/data-params.json'))['m_dir']
+        extract_benign(False)
+        extract_malware(fp, False)
+        construct_matrices(False, True, True, True)
+        result = evaluate(False)
+        try:
+            os.mkdir('./data/datasets/processed/results')
+        except:
+            pass
+        print('training metrics: ')
+        print(result[0])
+        print('testing metrics: ')
+        print(result[1])
+        result[0].to_csv('./data/datasets/processed/results/training.csv', index = False)
+        result[1].to_csv('./data/datasets/processed/results/testing.csv', index = False)
     return
 
 if __name__ == '__main__':
