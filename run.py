ce4406b628c787f439dfc77b16f21d182031b807
Shuibenyang Yuan <32397155+shy166@users.noreply.github.com>
2020-03-14 00:30:35 -0700

    cleanup

--- run.py
+++ run.py
@@ -7,17 +7,6 @@ from src.features import extract_benign, extract_malware
 from src.models import construct_matrices, evaluate, baseline
 from glob import glob
 def main(targets):
-    if 'metadata' in targets:
-        create_metadata()
-        return
-    if 'sampling-test' in targets:
-        cfg = cfg = json.load(open('./config/test-sample.json'))
-        sampling(**cfg)
-        return
-    if 'sampling' in targets:
-        cfg = json.load(open('./config/sample-params.json'))
-        sampling(**cfg)
-        return
     if 'data-test' in targets:
         cfg = json.load(open('./config/test-params.json'))
         get_data(**cfg)
@@ -26,30 +15,6 @@ def main(targets):
         fp = json.load(open('./config/test-params.json'))['m_dir']
         extract_benign(True)
         extract_malware(fp, True)
-        return
-    if 'matrix-test' in targets:
-        construct_matrices(True, True, True, True)
-        return
-    if 'baseline-test' in targets:
-        result = baseline.evaluate(True)
-        print('training metrics: ')
-        print(result[0])
-        print('testing metrics: ')
-        print(result[1])
-        return
-    if 'evaluate-test' in targets:
-        import pandas as pd
-        result = evaluate(True)
-        print('training metrics: ')
-        print(result[0])
-        print('testing metrics: ')
-        print(result[1])
-        try:
-            os.mkdir('./data/tests/processed/results')
-        except:
-            pass
-        result[0].to_csv('./data/tests/processed/results/training.csv', index = True)
-        result[1].to_csv('./data/tests/processed/results/testing.csv', index = True)
         return
     if 'test-project' in targets:
         cfg = json.load(open('./config/test-params.json'))
@@ -78,30 +43,6 @@ def main(targets):
         fp = json.load(open('./config/data-params.json'))['m_dir']
         extract_benign()
         extract_malware(fp)
-        return
-    if 'matrix' in targets:
-        construct_matrices(False, True, True, True)
-        return
-    if 'baseline' in targets:
-        result = baseline.evaluate(False)
-        print('training metrics: ')
-        print(result[0])
-        print('testing metrics: ')
-        print(result[1])
-        return
-    if 'evaluate' in targets:
-        import pandas as pd
-        result = evaluate(False)
-        print('training metrics: ')
-        print(result[0])
-        print('testing metrics: ')
-        print(result[1])
-        try:
-            os.mkdir('./data/datasets/processed/results')
-        except:
-            pass
-        result[0].to_csv('./data/datasets/processed/results/training.csv', index = True)
-        result[1].to_csv('./data/datasets/processed/results/testing.csv', index = True)
         return
     if 'project' in targets:
         cfg = json.load(open('./config/data-params.json'))
